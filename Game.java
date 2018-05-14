5ec18c0fe3060619a3e1b65d7696f67f688d1c52
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-05-14 14:16:25 +0200

    Minesweeper

--- Game.java
+++ Game.java
@@ -2,8 +2,19 @@
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[][] feld = spielfeldanlegen(3,3);
+		//feld = mineSetzen(0,3);
+		speilfeldanzeigen(feld);
+		}
+	public static String[][] spielfeldanlegen(int Zeilen, int Spalten) {
+		String[][] minefield = new String[Zeilen][Spalten];
+		for(int z = 0; z < Zeilen; z++) {
+			for(int s = 0; s < Spalten; s++) {
+				minefield[z][s] = "[ ]";
+			}
+		}
+		return minefield;
 	}
 
+	
 }
