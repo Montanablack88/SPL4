f919cb5d3ec6e1fc08b80523219aa644ba5bd2cf
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-05-14 14:36:38 +0200

    finish Spielfeldanzeigen

--- Game.java
+++ Game.java
@@ -1,20 +1,32 @@
 
 public class Game {
 
+	static int _zeilen = 3;
+	static int _spalten = 3;
+
 	public static void main(String[] args) {
-		String[][] feld = spielfeldanlegen(3,3);
-		//feld = mineSetzen(0,3);
-		speilfeldanzeigen(feld);
+		String[][] feld = spielfeldanlegen(3, 3);
+		// feld = mineSetzen(0,3);
+		spielfeldanzeigen(feld, _zeilen, _spalten);
+	}
+
+	public static void spielfeldanzeigen(String[][] feld, int Zeilen, int Spalten) {
+		for (int z = 0; z < Zeilen; z++) {
+			for (int s = 0; s < Spalten; s++) {
+				System.out.print(feld[z][s]);
+			}
+			System.out.println();
 		}
+	}
+
 	public static String[][] spielfeldanlegen(int Zeilen, int Spalten) {
 		String[][] minefield = new String[Zeilen][Spalten];
-		for(int z = 0; z < Zeilen; z++) {
-			for(int s = 0; s < Spalten; s++) {
+		for (int z = 0; z < Zeilen; z++) {
+			for (int s = 0; s < Spalten; s++) {
 				minefield[z][s] = "[ ]";
 			}
 		}
 		return minefield;
 	}
 
-	
 }
